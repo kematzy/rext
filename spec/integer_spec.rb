c250b7d3136c13b9d764e3227e53301901554987
TJ Holowaychuk <tj@vision-media.ca>
2009-03-23 10:36:13 -0700

    Refactored Integer#before / since specs

--- spec/integer_spec.rb
+++ spec/integer_spec.rb
@@ -6,15 +6,15 @@ describe Integer do
     
     describe "#ago / #before" do
       it "should return the distance in time before now, or specified time" do
-        event = Date.parse 'may 25th 1987'
-        15.days.before(event).should == Date.parse('may 15th 1987')
+        event = Time.mktime 1987, 5, 25
+        15.days.before(event).should == Time.mktime(1987, 5, 10)
       end
     end
 
     describe "#since / #from_now" do
       it "should return the distance in time from now, or specified time" do
-        event = Date.parse 'may 25th 1987'
-        5.days.since(event).should == Date.parse('may 30th 1987')
+        event = Time.mktime 1987, 5, 25
+        5.days.since(event).should == Time.mktime(1987, 5, 30)
       end
     end
     
