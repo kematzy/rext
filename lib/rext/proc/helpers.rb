75d7717568c131f6335709d9d13732a7ff904728
TJ Holowaychuk <tj@vision-media.ca>
2009-03-23 15:54:51 -0700

    Added Proc#yield_or_eval docs

--- lib/rext/proc/helpers.rb
+++ lib/rext/proc/helpers.rb
@@ -1,6 +1,20 @@
 
 class Proc
   
+  ##
+  # Yield or instance evaluate the +object+ passed
+  # based on this proc's arity.
+  #
+  # === Examples
+  #
+  #   def foo &block
+  #     block.yield_or_eval 'bar'
+  #   end
+  #
+  #   foo { |v| v.length }  # => 3
+  #   foo { length }        # => 3
+  #
+  
   def yield_or_eval object
     arity > 0 ? self.call(object) : object.instance_eval(&self)
   end
