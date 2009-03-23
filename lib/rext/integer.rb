0c1932d3015af7085f2f63fe275c049cf9377996
TJ Holowaychuk <tj@vision-media.ca>
2009-03-23 14:44:09 -0700

    Added Integer#ordanalize

--- lib/rext/integer.rb
+++ lib/rext/integer.rb
@@ -3,4 +3,5 @@
 # Load Integer specific rext extensions.
 #++
 
+require 'rext/integer/helpers'
 require 'rext/integer/time'
--- spec/integer_spec.rb
+++ spec/integer_spec.rb
@@ -2,6 +2,20 @@
 require 'rext/integer'
 
 describe Integer do
+  describe "helpers" do
+    describe "#ordanalize" do
+      it "should convert integers to an ordanal string" do
+        1.ordinalize.should == '1st'
+        2.ordinalize.should == '2nd'
+        3.ordinalize.should == '3rd'
+        4.ordinalize.should == '4th'
+        11.ordinalize.should == '11th'
+        12.ordinalize.should == '12th'
+        13.ordinalize.should == '13th'
+      end
+    end
+  end
+  
   describe "time" do
     
     describe "#ago / #before" do
