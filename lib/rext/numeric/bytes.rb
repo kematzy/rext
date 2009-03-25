ae261cc93bb638f39f38aec7fffe50c119d9ded6
TJ Holowaychuk <tj@vision-media.ca>
2009-03-25 15:13:33 -0700

    Refactored byte converisons

--- lib/rext/numeric/bytes.rb
+++ lib/rext/numeric/bytes.rb
@@ -1,29 +1,16 @@
 
 class Numeric
   
-  def bytes
-    self
-  end
+  def bytes; self end
+  def kilobytes; self * 1024 end
+  def megabytes; self * 1024.kilobytes end
+  def gigabytes; self * 1024.megabytes end
+  def terabytes; self * 1024.gigabytes end
+  
   alias :byte :bytes
-
-  def kilobytes
-    self * 1024
-  end
   alias :kilobyte :kilobytes
-
-  def megabytes
-    self * 1024.kilobytes
-  end
   alias :megabyte :megabytes
-
-  def gigabytes
-    self * 1024.megabytes 
-  end
   alias :gigabyte :gigabytes
-
-  def terabytes
-    self * 1024.gigabytes
-  end
   alias :terabyte :terabytes
   
 end
