c45e1f8a284d3d8f6d07f196d37f6d4dcefd1d88
TJ Holowaychuk <tj@vision-media.ca>
2009-03-25 16:45:49 -0700

    Meta doc

--- lib/rext/object/metaclass.rb
+++ lib/rext/object/metaclass.rb
@@ -1,14 +1,24 @@
 
 class Object
   
+  ##
+  # Return the metaclass of this object.
+  
   def metaclass
     class << self; self end
   end
+  
+  ##
+  # Evaluate a +string+ or +block+ in context to this
+  # object metaclass.
   
   def meta_eval string = nil, &block
     return metaclass.class_eval(string) if string
     metaclass.class_eval &block
   end
+  
+  ##
+  # Define a singleton method.
   
   def meta_def name, &block
     meta_eval { define_method name, &block }
