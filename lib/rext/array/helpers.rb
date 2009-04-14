c2b361d7eee1d06f8ec28f00feb81a7b19582ed5
visionmedia <tj@vision-media.ca>
2009-04-13 17:36:02 -0700

    Added Array#chunk/in_groups_of

--- lib/rext/array/helpers.rb
+++ lib/rext/array/helpers.rb
@@ -15,4 +15,17 @@ class Array
     self[0..position]
   end
   
+  ##
+  # Split an array into +n+ chunks. When a +block+
+  # is present each chunk will be passed to it.
+  
+  def chunk n, fill_with = nil, &block
+    (0..n-1).inject [] do |chunks, i|
+      chunk = slice i * n, n
+      yield chunk if block
+      chunks << chunk
+    end
+  end
+  alias :in_groups_of :chunk
+  
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -20,5 +20,19 @@ describe Array do
       end
     end
     
+    describe "#chunk" do
+      it "should split an array into several containing the length of n" do
+        (1..9).to_a.chunk(3).should == [[1,2,3], [4,5,6], [7,8,9]]
+      end
+      
+      it "should accept a block, yielding each chunk" do
+        chunks = []
+        (1..9).to_a.in_groups_of(3) do |chunk|
+          chunks << chunk
+        end
+        chunks.should == [[1,2,3], [4,5,6], [7,8,9]]
+      end
+    end
+    
   end
 end
