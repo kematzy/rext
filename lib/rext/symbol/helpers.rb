a732f04d2b75eda6953343463bb9956dcf498ecd
visionmedia <tj@vision-media.ca>
2009-08-24 10:51:27 -0700

    Examples

--- lib/rext/symbol/helpers.rb
+++ lib/rext/symbol/helpers.rb
@@ -1,5 +1,15 @@
 
 class Symbol
+  
+  ##
+  # Return a proc which sends itself as a message
+  # to the first proc argument.
+  #
+  # === Examples
+  #
+  #  %w( some foo bar ).map(&:length)  # => [4, 3, 3]
+  #
+  
   def to_proc
     Proc.new { |object| object.send self }
   end
